aa9cec3be5dc480222c815a42e2a6484bee1bc3c
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-06-09 17:15:59 -0700

    Revert "changed lambda handler to an API"
    This reverts commit 6e0e95b0a3943e66754c143cffed63fd1d74b192.

--- app.py
+++ app.py
@@ -16,27 +16,10 @@ import json
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
 
-def lambda_handler(event, context):
-    try:
-        main()
-        return {
-            "statusCode": 200,
-            "body": json.dumps({
-                "message": "hello everyone",
-                # "location": ip.text.replace("\n", "")
-            }),
-        }
-    except:
-        return {
-            "statusCode": 400,
-            "body": json.dumps({
-                "message": "error out",
-                # "location": ip.text.replace("\n", "")
-            }),
-        }
+def lambda_handler (event, context):
+  main()
 
-
-def check_sender_of_last_thread(thread):
+def check_sender_of_last_thread(thread): 
     if 'messages' in thread:
         last_message = thread['messages'][-1]
     if 'payload' in last_message:
@@ -48,55 +31,43 @@ def check_sender_of_last_thread(thread):
                 return sender
     return None
 
-
 def main():
-    not_replied_emails()
-
+	not_replied_emails()
+  
 
 
 def not_replied_emails():
-    # Variable creds will store the user access token.
-    # If no valid token found, we will create one.
-    creds = None
+	# Variable creds will store the user access token.
+	# If no valid token found, we will create one.
+	creds = None
 
-    # The file token.pickle contains the user access token.
-    # Check if it exists
-    if os.path.exists('token.pickle'):
+	# The file token.pickle contains the user access token.
+	# Check if it exists
+	if os.path.exists('token.pickle'):
 
-        # Read the token from the file and store it in the variable creds
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
+		# Read the token from the file and store it in the variable creds
+		with open('token.pickle', 'rb') as token:
+			creds = pickle.load(token)
 
-    # If credentials are not available or are invalid, ask the user to log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'gmail_oath.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+	# If credentials are not available or are invalid, ask the user to log in.
+	if not creds or not creds.valid:
+		if creds and creds.expired and creds.refresh_token:
+			creds.refresh(Request())
+		else:
+			flow = InstalledAppFlow.from_client_secrets_file('gmail_oath.json', SCOPES)
+			creds = flow.run_local_server(port=0)
 
-        # Save the access token in token.pickle file for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+		# Save the access token in token.pickle file for the next run
+		with open('token.pickle', 'wb') as token:
+			pickle.dump(creds, token)
 
-    # Connect to the Gmail API
-    service = build('gmail', 'v1', credentials=creds)
-    print('Connection Established')
+	# Connect to the Gmail API
+	service = build('gmail', 'v1', credentials=creds)
+	print('Connection Established')
 
-    labels_results = service.users().labels().list(userId='me').execute()
-    labels = labels_results.get('labels', [])
 
-    # #store all ids with follow-up label
-    follow_up_label_id = None
-    for label in labels:
-        if label['name'] == 'Follow-up':
-            follow_up_label_id = label['id']
-    # request a list of all the messages
-    # We can also pass maxResults to get any number of emails. Like this:
-    result = service.users().messages().list(
-        maxResults=30, userId='me', labelIds=['SENT']).execute()
-    messages = result.get('messages')
+	labels_results = service.users().labels().list(userId='me').execute()
+	labels = labels_results.get('labels', [])
 
 	# #store all ids with follow-up label
 	follow_up_label_id = None
@@ -108,16 +79,7 @@ def not_replied_emails():
 	result = service.users().messages().list(maxResults=50, userId='me', labelIds=['SENT']).execute()
 	messages = result.get('messages')
 
-    # iterate through all the messages
-    counter = 0
-    for msg in messages:
-        # Get the message from its id
-        txt = service.users().messages().get(
-            userId='me', id=msg['id']).execute()
-        # print("txt['labelIds']", txt['labelIds'])
-        if follow_up_label_id in txt['labelIds']:
-            target_text = txt
-            print("Target text", target_text)
+	# messages is a list of dictionaries where each dictionary contains a message id.
 
 	# iterate through all the messages
 	thread_ids = set()
