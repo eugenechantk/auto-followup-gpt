6b009c1af16a4ded0b185e0cb0653c70bed8adeb
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-06-24 20:17:12 -0700

    added token_uri, client id and client secret for fetching refresh token

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import json
 from google.oauth2.credentials import Credentials
 import requests
+import os
 
 # Import all the logic functions from logic.py
 import logic
@@ -10,6 +11,8 @@ import auth
 
 
 def generate_follow_up_handler(event, context):
+    oauth_client_id = os.environ['OAUTH_CLIENT_ID']
+    oauth_client_secret = os.environ['OAUTH_CLIENT_SECRET']
     print('generate follow up lambda function invoked')
     # Parse the request body
     request_body = json.loads(event['body'])
@@ -19,10 +22,10 @@ def generate_follow_up_handler(event, context):
         accessToken = request_body['access_token']
         callbackUrl = request_body['callback_url']
         refreshToken = request_body['refresh_token']
-        print(accessToken, callbackUrl)
+        print(request_body)
         if accessToken is None:
             raise CustomError(400, 'NoCredentials', 'No access token provided')
-        creds = Credentials(token=accessToken, refresh_token=refreshToken)
+        creds = Credentials(token=accessToken, refresh_token=refreshToken, client_id=oauth_client_id, client_secret=oauth_client_secret, token_uri='https://oauth2.googleapis.com/token')
 
         # Get the user's email address
         email_address = auth.get_user_email(creds)
